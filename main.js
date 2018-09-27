dc9a2e44e61c74bf3e4d05704bbac1520f6a098e
Bjornhona <asaeri3@hotmail.com>
2018-09-27 10:13:41 +0200

    files created

---
+++ main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function main () {
+
+console.log('Midnight Hunter - game starts');
+
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+buildSplash() {
+
+};
+
+destroySplash() {
+
+};
+
+buildGame() {
+
+};
+
+destroyGame() {
+
+};
+
+buildGameOver() {
+
+};
+
+destroyGameOver() {
+  
+};
+
+
+}
